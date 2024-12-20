ee9a4949875b04dfc7fe11239ad39b7bbb819f08
dtoinagn <tiandn@gmail.com>
2024-12-19 22:50:10 -0500

    Add rate throttling

--- server/bookssite/api/views.py
+++ server/bookssite/api/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, throttle_classes
 from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle
 from rest_framework import status
 from .models import Book
 from .serializer import BookSerializer
 
+class OneHundredsPerDayThrottle(UserRateThrottle):
+    rate = '100/day'
+
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f'{func.__name__} is called')
+        return func(*args, **kwargs)
+    return wrapper
+
 @api_view(['GET'])
+@throttle_classes([OneHundredsPerDayThrottle])
 def get_books(request):
     books = Book.objects.all()
     serializedData = BookSerializer(books, many=True).data
     return Response(serializedData)
 
 @api_view(['POST'])
+@throttle_classes([OneHundredsPerDayThrottle])
 def create_book(request):
     data = request.data
     serializer = BookSerializer(data = data)
@@ -22,6 +34,7 @@ def create_book(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT','DELETE'])
+@throttle_classes([OneHundredsPerDayThrottle])
 def book_detail(request, pk):
     try:
         book = Book.objects.get(pk=pk)
